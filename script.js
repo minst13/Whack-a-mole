625cf04fc6575b55bc75fbb3bff4382cc33b24eb
minst13 <minamthapa38@yahoo.co.uk>
2024-08-14 18:56:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const startGame = document.getElementById("start");
+
+
+const holes = document.querySelectorAll(".hole");
+const moles = document.querySelectorAll(".mole");
+const scoreCount = document.querySelector(".score");
+let lastHole;
+let tally = 0;
+let timeUp = false;
+
+
+function randTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randHole(holes) {
+    const molePop = Math.floor(Math.random() * holes.length);
+    const hole = holes[molePop];
+    if (hole == lastHole) {
+        console.log("You got same hole!")
+        return randHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+
+function showMole() {
+    const time = randTime(250, 1000);
+    const hole = randHole(holes);
+    console.log(time, hole)
+    hole.classList.add("up");
+    setTimeout(() => {
+        hole.classList.remove("up");
+        if (!timeUp) showMole();
+    }, time);
+}
+
+startGame.addEventListener("click",
+    function () {
+        gameStart();
+    }
+)
+
+function gameStart() {
+    scoreCount.innerText = 0;
+    timeUp = false;
+    tally = 0;
+    showMole();
+    setTimeout(() => timeUp = true, 10000)
+}
+
+function hit(e) {
+    console.log(e)
+    if (!e.isTrusted) return;
+    tally++
+    this.classList.remove("up");
+    scoreCount.textContent = tally;
+}
+
+moles.forEach(mole => mole.addEventListener('click', hit));
